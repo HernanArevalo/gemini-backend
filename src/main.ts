3c2fd32afb6324bfbf4b12b21e61dfb914db0932
Hernán Arévalo <hernanarevalo16@gmail.com>
2026-02-18 14:30:36 -0300

    feat(cors): enable CORS with specified methods and headers

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
