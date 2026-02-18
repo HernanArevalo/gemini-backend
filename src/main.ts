d9f6b8b3179a3b9482399f21eb39ef2c57e857e9
Hernán Arévalo <hernanarevalo16@gmail.com>
2026-02-18 14:35:20 -0300

    feat(cors): specify CORS origin for local development

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: true,
+    origin: 'http://localhost:8081',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
